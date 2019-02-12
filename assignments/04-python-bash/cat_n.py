a39244828481344a8c29f35569a2d375e6928613
Kai Blumberg <kblumberg@email.arizona.edu>
2019-02-11 23:36:14 -0700

    finished cat_n and head all tests passing

--- assignments/04-python-bash/cat_n.py
+++ assignments/04-python-bash/cat_n.py
@@ -7,9 +7,17 @@ import sys
 args = sys.argv[1:]
 
 if len(args) != 1:
-    print('Usage: {} ARG'.format(os.path.basename(sys.argv[0])))
+    print('Usage: {} FILE'.format(os.path.basename(sys.argv[0])))
     sys.exit(1)
 
-arg = args[0]
+in_arg = args[0]
 
-print('Arg is "{}"'.format(arg))
+if not os.path.isfile(in_arg):
+    print('{} is not a file'.format(in_arg))
+    sys.exit(1)
+
+with open (in_arg, 'r') as in_file:
+    for count, line in enumerate(in_file, start=1):
+        print(' {}: {}'.format(count, line[:-1]))
+
+#print('Arg is "{}"'.format(arg))
--- assignments/04-python-bash/head.py
+++ assignments/04-python-bash/head.py
@@ -6,10 +6,37 @@ import sys
 
 args = sys.argv[1:]
 
-if len(args) != 1:
-    print('Usage: {} ARG'.format(os.path.basename(sys.argv[0])))
+if len(args) < 1:
+    print('Usage: {} FILE [NUM_LINES]'.format(os.path.basename(sys.argv[0])))
     sys.exit(1)
 
-arg = args[0]
+in_arg = args[0]
 
-print('Arg is "{}"'.format(arg))
+if not os.path.isfile(in_arg):
+    print('{} is not a file'.format(in_arg))
+    sys.exit(1)
+
+if len(args) >= 2:
+    max_count = int(args[1])
+else:
+    max_count = 3
+    
+
+if max_count <= 0:
+        print('lines ({}) must be a positive number'.format(max_count))
+        sys.exit(1)
+
+with open (in_arg, 'r') as in_file:
+    for count, line in enumerate(in_file, start=1):
+        if count==max_count:
+            print('{}'.format(line[:-1]))
+            break
+        else:
+            print('{}'.format(line[:-1]))
+
+
+
+#print(max_count)
+
+
+#print('Arg is "{}"'.format(arg))
