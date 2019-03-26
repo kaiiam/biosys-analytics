f91f1f6b4782209fce882e653968c1aeb613adf9
Kai Blumberg <kblumberg@email.arizona.edu>
2019-03-25 19:30:50 -0700

    added blackjack

---
+++ assignments/10-blackjack/blackjack.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Author : kblumberg
+Date   : 2019-03-25
+Purpose: Rock the Casbah
+"""
+
+import os
+import sys
+
+
+# --------------------------------------------------
+def main():
+    args = sys.argv[1:]
+
+    if len(args) != 1:
+        print('Usage: {} ARG'.format(os.path.basename(sys.argv[0])))
+        sys.exit(1)
+
+    arg = args[0]
+
+    print('Arg is "{}"'.format(arg))
+
+
+# --------------------------------------------------
+main()
