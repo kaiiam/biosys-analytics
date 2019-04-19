3d6a71bc305fc1acc404b48d20e49765c7ce9813
Kai Blumberg <kblumberg@email.arizona.edu>
2019-04-18 17:05:10 -0700

    Fix hamm.py to use logging and pass tests

--- assignments/13-hamm/hamm.py
+++ assignments/13-hamm/hamm.py
@@ -18,7 +18,7 @@ def get_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument(
-        'positional', metavar='FILE', help='File inputs', nargs=2)
+        'files', metavar='FILE', help='File inputs', nargs=2)
 
 
     parser.add_argument(
@@ -43,50 +43,13 @@ def die(msg='Something bad happened'):
 # --------------------------------------------------
 def dist(s1, s2):
     """Haming distance calculation function"""
-    m = 0
-    l1 = s1.split()
-    l2 = s2.split()
-    #print(l1)
 
-    # for i, x in enumerate(l1):
-    #     compare1 = x
-    #     compare2 = l2[i]
-    #     for j, y in enumerate(compare1):
-    #         #print(y)
-    #         if len(compare1) >= len(compare2):
-    #             if y != compare2[j]:
-    #                 m += 1
-    #         else:
-    #             if compare2[j] != y:
-    #                 m +=1
-
-
-    for i, x in enumerate(l1):
-        if len(l1[i]) >= len(l2[i]):
-            #print('l1 longer')
-            compare1 = l1[i]
-            compare2 = l2[i]
-        else:
-            #print('l2 longer')
-            compare1 = l2[i]
-            compare2 = l1[i]
-
-        #print(compare1, compare2)
-        for j, y in enumerate(compare2):
-            if compare1[j] != y:
-                m +=1
-            #print(y)
-        for num in range(0, len(compare1) - len(compare2)):
-            #print(num)
-            m +=1
-
-    #print(m)
-
-    # for i, x in enumerate(l1):
-    #     if x != l2[i]:
-    #         m += 1
-    #return(m)
-    return(m)
+    dist = abs(len(s1) - len(s2))
+    for c1, c2 in zip(s1, s2):
+        if c1 != c2:
+            dist +=1
+    logging.debug('s1 = {} s2 = {} d = {}'.format(s1, s2, dist))
+    return(dist)
 
 
 # --------------------------------------------------
@@ -94,40 +57,32 @@ def main():
     """Make a jazz noise here"""
     args = get_args()
     debug_arg = args.debug
-    pos_arg = args.positional
+    f1, f2 = args.files
 
     logging.basicConfig(
-    filename='.log',
-    filemode='w',
-    level=logging.DEBUG if args.debug else logging.CRITICAL
-    )
+        filename='.log',
+        filemode='w',
+        level=logging.DEBUG if args.debug else logging.CRITICAL
+        )
 
-    for x in pos_arg:
+    logging.debug('file1 = {}, file1 = {}'.format(f1,f2))
+
+    for x in args.files:
         if not os.path.isfile(x):
             print('"{}" is not a file'.format(x), file=sys.stderr)
             sys.exit(1)
 
-    s1 = ''
-    with open(pos_arg[0], 'r') as f:
-        for line in f:
-            s1 += line
 
-    s2 = ''
-    with open(pos_arg[1], 'r') as f:
-        for line in f:
-            s2 += line
+    words1 = open(f1).read().split()
 
-    #print(s2)
-    m = dist(s1, s2)
-    print(m)
+    words2 = open(f2).read().split()
 
-    # tests = [('foo', 'boo', '1'), ('foo', 'faa', '2'), ('foo', 'foobar', '3'),
-    #                  ('TAGGGCAATCATCCGAG', 'ACCGTCAGTAATGCTAC',
-    #                   '9'), ('TAGGGCAATCATCCGG', 'ACCGTCAGTAATGCTAC', '10')]
-    #
-    # for s1, s2, n in tests:
-    #     d = dist(s1, s2)
-    #     print(d, n)
+    dist_count = 0
+
+    for w1, w2 in zip(words1, words2):
+        dist_count += dist(w1, w2)
+
+    print(dist_count)
 
 
 # --------------------------------------------------
